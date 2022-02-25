2d07480d30721873de2ac1126ae42163a3fc942b
OpsecGuy <30886720+OpsecGuy@users.noreply.github.com>
2022-02-25 14:33:14 +0100

    Code style changes

--- pid.py
+++ pid.py
@@ -7,34 +7,38 @@ class Spoofer():
         self.reg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
         self.key = winreg.OpenKey(self.reg, self.path, access=winreg.KEY_ALL_ACCESS)
         self.keys_len = winreg.QueryInfoKey(self.key)[1]
-        self.pID = winreg.QueryValueEx(self.key, 'ProductId')[0]
+        self.pid = winreg.QueryValueEx(self.key, 'ProductId')[0]
         print('Spoofer Initialized')
 
     def make_copy(self) -> None:
         with open('pid_backup_key.txt', 'w+') as file:
-            file.write(self.pID)
+            file.write(self.pid)
             print('Key backup created!')
         file.close()
 
-    def set_value(self) -> None:
-        strings = ['A',
+    def get_random(self) -> None:
+        chars = ['A',
         'B', 'C', 'D', 'E', 'F','G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'U', 'P', 'R', 'S', 'T', 'W', 'Y', 'Z',
         '1','2', '3', '4', '5', '6', '7', '8', '9', '0']
         numbers = ['1','2', '3', '4', '5', '6', '7', '8', '9', '0']
-        
-        val1 = ''.join(random.sample(numbers, 5))
-        val2 = ''.join(random.sample(numbers, 5))
-        val3 = ''.join(random.sample(numbers, 5))
-        val4 = ''.join(random.sample(strings, 5))
-        
-        new_pID = f'{val1}-{val2}-{val3}-{val4}'
+
+        v0 = ''.join(random.sample(numbers, 5))
+        v1 = ''.join(random.sample(numbers, 5))
+        v2 = ''.join(random.sample(numbers, 5))
+        v3 = ''.join(random.sample(chars, 5))
+
+        new_pid = f'{v0}-{v1}-{v2}-{v3}'
+        return new_pid
+
+    def set_value(self) -> None:
+        new_key = self.get_random()
         try:
-            if self.pID != new_pID:
-                winreg.SetValueEx(self.key, 'ProductId', 0, winreg.REG_SZ, new_pID)
+            if self.pid != new_key:
+                winreg.SetValueEx(self.key, 'ProductId', 0, winreg.REG_SZ, new_key)
         except Exception as err:
             print(err)
 
-        print(f'Old key: {self.pID}\nNew key: {new_pID}')
+        print(f'Old key: {self.pid}\nNew key: {new_key}')
 
 
 if __name__ == '__main__':
